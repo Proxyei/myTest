ff068ee2ce5fec94cde2c815e6e46b277d470594
proxyei <insoul_net@qq.com>
2017-03-18 12:35:03 +0800

    Initial commit 第一个工程

---
+++ hello-world/src/com/xywei/test/Helloworld.java
@@ -0,0 +1,10 @@
+package com.xywei.test;
+
+/**
+ * Created by wodoo on 2017/3/18.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+    }
+}
